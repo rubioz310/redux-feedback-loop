63a1ca6b83f2466be612b76078f7bca8d511222f
Jose Rubio <ru310z@gmail.com>
2021-07-17 15:25:30 -0500

    Created feedbackRouter, linked it with the server and added a POST route inside it

---
+++ server/routes/feedbackRouter.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+// router.put('/', (req, res) => {
+//     const galleryId = req.params.id;
+//     let count = req.body.like;
+//     let sqlText = `UPDATE "feedback" 
+//                 SET "likes" = "likes" + $1
+//                 WHERE "id" = $2`;
+    
+//     pool.query(sqlText, [count, galleryId])
+//     .then(result => {
+//         res.sendStatus(202);
+//     }).catch(error => {
+//         console.log("Error liking feedback", error);
+//     })
+// }); // END PUT Route
+
+// GET Route that gets all feedback from the gallery
+// router.get('/', (req, res) => {
+//     const sqlText = 'SELECT * FROM "feedback" ORDER BY "id" ASC'
+
+//     pool.query(sqlText)
+//     .then(result => {
+//         res.send(result.rows);
+//     }).catch(error => {
+//         console.log("Error getting feedback.", error);
+//     })
+// }); // END GET Route
+
+
+//POST Route for uploading feedback
+router.post('/upload', (req, res) => {
+    const {
+        feeling,
+        understanding,
+        support,
+        comments
+    } = req.body;
+    let sqlText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                VALUES ($1, $2, $3, $4)`;
+    pool.query(sqlText, [feeling, understanding, support, comments])
+    .then(result => {
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log("Error uploading feedback", error);
+    })
+}); //END POST Route
+
+// DELETE Route that deletes feedback
+// router.delete('/delete/:id', (req, res) => {
+//     const galleryId = req.params.id;
+//     let sqlText = `DELETE FROM "feedback" 
+//                 WHERE "id" = $1`;
+    
+//     pool.query(sqlText, [galleryId])
+//     .then(result => {
+//         res.sendStatus(202);
+//     }).catch(error => {
+//         console.log("Error deleting feedback", error);
+//     })
+// }); // END DELETE Route
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const feedback = require('./routes/feedbackRouter')
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/feedback', feedback);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
