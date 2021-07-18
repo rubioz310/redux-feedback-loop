aaa6bf39442f38f1ea7f5837f9b3c7462a34626d
Jose Rubio <ru310z@gmail.com>
2021-07-17 22:26:31 -0500

    Added styling to  commentsForm and SupportForm

--- src/components/Feedback/CommentsForm/CommentsForm.js
+++ src/components/Feedback/CommentsForm/CommentsForm.js
@@ -1,43 +1,73 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { useSelector } from "react-redux"
-import { Fade } from '@material-ui/core';
+
+//Material-ui imports
+import { Paper, Slide } from '@material-ui/core';
+import { Mood, MoodBad } from "@material-ui/icons";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 function CommentsForm(){
     const history = useHistory();
     const dispatch = useDispatch();
-    
+    const {direction} = useParams();
+
+    //This will show the icons at the extremes of the slider as long as marks for each value
+    const marks = [
+        {
+            value: 1,
+            label: <MoodBad />
+        },
+        { value : 2},
+        { value : 3},
+        { value : 4},
+        {
+            value: 5,
+            label: <Mood />
+        }
+    ]
+
+    //This will set current value of the rate if it has one otherwise it will default to ""
     const feedback = useSelector(store => store.feedbackReducer);
-    //This will set current value of the comments if it has one otherwise it will default to ""
-    const [comments, setComments] = useState(feedback.comments ? feedback.comments: "");
+    const [commentsRate, setCommentsRate] = useState(feedback.comments ? feedback.comments: "");
 
     const handleNext = () => {
         dispatch({
             type: 'CHANGE_COMMENTS',
-            payload: comments
+            payload: commentsRate
         })
-        history.push('/reviewScreen');
+        history.push('/reviewScreen/left');
     }
     const handlePrevious = () => {
         dispatch({
             type: 'CHANGE_COMMENTS',
-            payload: comments
+            payload: commentsRate
         })
-        history.push('/supportForm');
+        history.push('/supportForm/right');
     }
 
     return (
-        <div>
-            <Fade in={true}>
+        <Slide in={true} direction={direction}>
+            <Paper className="formContainer" elevation={6}>
+                <h3>Any comments you want to leave?</h3>
                 <div>
-            <p>Any comments you want to leave?</p>
-            <textarea value={comments} onChange={event => setComments(event.target.value)}/><br/>
-            <button onClick={handlePrevious}>Previous</button>
-            <button onClick={handleNext}>Next</button>
-            </div>
-            </Fade>
-        </div>
+                    <TextField
+                        label="Comments"
+                        multiline
+                        variant="filled"
+                        value={commentsRate}
+                        maxRows="2"
+                        fullWidth="true"
+                        autoFocus="true"
+                        onChange={event => setCommentsRate(event.target.value)}
+                    />
+                </div>
+                <div className="previousBtn"><Button variant="contained" color="primary" size="small" onClick={handlePrevious}>Back</Button></div>
+                <div className="nextBtn"><Button variant="contained" color="primary" size="small" onClick={handleNext}>Next</Button></div>
+            </Paper>
+        </Slide>
     )
 }
 
